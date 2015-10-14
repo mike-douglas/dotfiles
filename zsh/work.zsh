89e01fe725b73ac3a235b860f1f59bc300b20719
Michael Douglas <mdouglas@npr.org>
2015-10-14 10:12:59 -0400

    Helpers for moving around environments at work

--- zsh/work.zsh
+++ zsh/work.zsh
@@ -4,4 +4,14 @@ if [ -d ${HOME}/.oh-my-zsh ]; then
     plugins=(git python osx)
 fi
 
+ssh-add ~/.ssh/id_rsa 1>/dev/null 2>&1
+
+function qalan_run {
+    ssh -A dev1 "ssh -A ${@}"
+}
+
+function prodlan_run {
+    ssh -A dev1 ssh -A ns2 "ssh -A ${@}"
+}
+
 alias ssh="ssh -A"
