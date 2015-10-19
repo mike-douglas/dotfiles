458bcb14d27ad55f5259e3bd6c15a0388aea4b9c
Michael Douglas <mdouglas@npr.org>
2015-10-19 11:24:26 -0400

    That port range is a little ridiculous

--- zsh/include/docker.zsh
+++ zsh/include/docker.zsh
@@ -5,7 +5,7 @@ fi
 
 # You should only need to do this once! But now you never need to google for it again <3
 function docker_vm_map_ports {
-    for i in {8000..9999}; do
+    for i in {8080..8100}; do
         echo "Mapping ${i} -> ${i}"
         VBoxManage modifyvm "default" --natpf1 "tcp-port$i,tcp,,$i,,$i";
         VBoxManage modifyvm "default" --natpf1 "udp-port$i,udp,,$i,,$i";
