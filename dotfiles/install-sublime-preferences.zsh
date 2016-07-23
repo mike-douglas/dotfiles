e8c2ab22fec8137a3a6093ca997b3ce6e5185736
Mike Douglas <michael.j.douglas@gmail.com>
2016-07-23 09:18:12 -0400

    Back to Sublime, here are some preferences..

--- dotfiles/install-sublime-preferences.zsh
+++ dotfiles/install-sublime-preferences.zsh
@@ -1,11 +1,18 @@
 #!/bin/zsh
 
 if [ -d "/Applications/Sublime Text.app" ]; then
-    SUBLIME_USER_PACKAGE="${HOME}/Library/Application Support/Sublime Text 3/Packages/User/"
+    PACKAGE_CONTROL="https://sublime.wbond.net/Package Control.sublime-package"
+    SUBLIME="${HOME}/Library/Application Support/Sublime Text 3/"
+    SUBLIME_INSTALLED_PACKAGES="${SUBLIME}/Installed Packages/"
+    SUBLIME_USER_PACKAGE="${SUBLIME}/Packages/User/"
 
+    mkdir -p "${SUBLIME_INSTALLED_PACKAGES}"
     mkdir -p "${SUBLIME_USER_PACKAGE}"
 
+    echo "-- Installing Package Control" && \
+        curl "${PACKAGE_CONTROL}" > "${SUBLIME_INSTALLED_PACKAGES}/Package Control.sublime-package"
+
     for file in ${HOME}/.home/sublime/*; do
-        ln -s "${file}" "${SUBLIME_USER_PACKAGE}"
+        cp "${file}" "${SUBLIME_USER_PACKAGE}"
     done
 fi
