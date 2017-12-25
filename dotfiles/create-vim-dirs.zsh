775038ec564485dc5c0ad7059ff3d14b93d23896
Mike Douglas <michaeldouglas@invisionapp.com>
2017-12-25 14:07:37 -0500

    Adding vim 8 configuration

--- dotfiles/create-vim-dirs.zsh
+++ dotfiles/create-vim-dirs.zsh
@@ -1,3 +1,8 @@
 #!/bin/zsh
 
 mkdir -p ${HOME}/.tmp/vim_backup ${HOME}/.tmp/vim_swap
+
+if [ ${#$(command -v vim)} != 0]; then
+    mkdir -p ${HOME}/.vim/pack/minpac/opt/minpac
+    git clone git@github.com:k-takata/minpac.git ${HOME}/.vim/pack/minpac/opt/minpac
+fi
