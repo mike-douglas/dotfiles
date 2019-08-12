04c09221f2ba5647d75386f81b08cef3d4856c9a
Mike Douglas <mike.douglas@insuremytrip.com>
2019-08-12 18:19:10 -0400

    Fixing syntax error

--- dotfiles/create-vim-dirs.zsh
+++ dotfiles/create-vim-dirs.zsh
@@ -2,7 +2,7 @@
 
 mkdir -p ${HOME}/.tmp/vim_backup ${HOME}/.tmp/vim_swap
 
-if [ ${#$(command -v vim)} != 0]; then
+if [ ${#$(command -v vim)} != 0 ]; then
     mkdir -p ${HOME}/.vim/pack/minpac/opt/minpac
     git clone git@github.com:k-takata/minpac.git ${HOME}/.vim/pack/minpac/opt/minpac
 fi
