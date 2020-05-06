fa3fc59ebcac43b1a526ea13bb65e82f44b377d4
Mike Douglas <mike.douglas@insuremytrip.com>
2020-05-06 11:00:37 -0400

    Adding laptop and work VM alias

--- zsh/work.zsh
+++ zsh/work.zsh
@@ -5,3 +5,5 @@ if [ -d ${HOME}/.oh-my-zsh ]; then
 fi
 
 ssh-add ~/.ssh/id_rsa 1>/dev/null 2>&1
+
+alias restartnetwork="vboxmanage natnetwork stop --netname IMT_VM_NAT && vboxmanage natnetwork start --netname IMT_VM_NAT"
