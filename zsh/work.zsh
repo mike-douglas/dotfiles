4069f7545f448f018cd8e7af502708dc28e4cfd9
Mike Douglas <mike.douglas@insuremytrip.com>
2021-08-06 10:34:51 -0400

    Adding some work shortcuts

--- zsh/work.zsh
+++ zsh/work.zsh
@@ -4,6 +4,14 @@ if [ -d ${HOME}/.oh-my-zsh ]; then
     plugins=(git python osx)
 fi
 
+function phpcheck() {
+    docker run --rm -v $(pwd):/app -t mdouglas/php-cs phpcs --standard=PSR12 -v ${@}
+}
+
+function phpfix() {
+    docker run --rm -v $(pwd):/app -t mdouglas/php-cs phpcbf --standard=PSR12 -v ${@}
+}
+
 ssh-add ~/.ssh/id_rsa 1>/dev/null 2>&1
 
 alias restartnetwork="vboxmanage natnetwork stop --netname IMT_VM_NAT && vboxmanage natnetwork start --netname IMT_VM_NAT"
