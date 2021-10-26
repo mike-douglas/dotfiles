a06d70b27add49052b4b6bdc4c45ba95b55db31e
Mike Douglas <michael.j.douglas@gmail.com>
2021-10-26 19:26:11 -0400

    Upding VSCode settings

--- dotfiles/install-code-preferences.zsh
+++ dotfiles/install-code-preferences.zsh
@@ -4,18 +4,8 @@ OS=$(uname)
 
 if [ "${OS}" = "Darwin" ]; then
     echo "      CONFIGURING VS CODE"
-
-    if [ ${#$(command -v code)} != 0 ]; then
-        # Install packages
-        cat ${HOME}/.home/code/extensions.list | xargs -n1 code --install-extension
-    fi
-
-    CODE_INSTALL="${HOME}/Library/Application Support/Code/User/"
-
-    for file in ${HOME}/.home/code/user/*.json; do
-        ln -fs ${file} ${CODE_INSTALL}
-    done
-
+    echo "      Settings Sync now handles all settings, extensions, and keybinds. Sign in using GitHub to complete this install"
+    
     # Set up repeat-on-hold for vim plugin in vscode
     defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 fi
