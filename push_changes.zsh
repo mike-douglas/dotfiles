49e7053be241cbcc47e9760a95903b6e45d63991
Mike Douglas <michael.j.douglas@gmail.com>
2022-04-07 06:19:53 -0400

    Fixing push_changes script

--- push_changes.zsh
+++ push_changes.zsh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-ssh ${1} rm -rf .home && ssh ${1} mkdir -p .home && git archive master | ssh ${1} tar -vx -C .home
+ssh ${1} rm -rf .home && ssh ${1} mkdir -p .home && git archive main | ssh ${1} tar -vx -C .home
